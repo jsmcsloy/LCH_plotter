b1b7e1ef7bd4421a9a18081b09c725781183ca84
Jamie MCsloy <jmcsloy@gmail.com>
2024-04-24 21:08:40 +0000

    d

--- app/app.py
+++ app/app.py
@@ -4,26 +4,39 @@ import plotly.express as px
 
 st.title("Simple plotter for LCH values")
 
-#side bar - capture L,C,H and insert into dataframe
+# Side bar - capture L, C, H and insert into dataframe
 L = st.sidebar.number_input("Enter L value")
 C = st.sidebar.number_input("Enter C value")
 H = st.sidebar.number_input("Enter H value")
 
-#load csv of data points
-data = st.sidebar.file_uploader("load in the csv file......")
+# Button to add LCH values into a list
+if 'data_list' not in st.session_state:
+    st.session_state.data_list = []
 
-df = data
+if st.sidebar.button("Add LCH value"):
+    st.session_state.data_list.append({'L': L, 'C': C, 'H': H})
+    st.sidebar.success("Added: L={}, C={}, H={}".format(L, C, H))
 
+# Load CSV of data points
+data_file = st.sidebar.file_uploader("Load in the CSV file...")
 
-try:
-    fig = px.scatter_polar(df, r="C", theta="H", direction='counterclockwise', start_angle=0)
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(showticklabels=False)  # Hides the radial axis tick labels
+if data_file is not None:
+    df = pd.read_csv(data_file)
+else:
+    # If no file uploaded yet, use accumulated data
+    df = pd.DataFrame(st.session_state.data_list)
+
+# Plotting only if dataframe is not empty
+if not df.empty:
+    try:
+        fig = px.scatter_polar(df, r="C", theta="H", direction='counterclockwise', start_angle=0)
+        fig.update_layout(
+            polar=dict(
+                radialaxis=dict(showticklabels=False)  # Hides the radial axis tick labels
+            )
         )
-    )
-
-    fig.show()
-
-except Exception as e:
-    print(e)
+        st.plotly_chart(fig)
+    except Exception as e:
+        st.error("Error generating plot: {}".format(e))
+else:
+    st.info("Upload data or add LCH values to generate plot.")
