e5413899494cef71c04af61b823735dd1c258a48
Jamie MCsloy <jmcsloy@gmail.com>
2024-04-27 14:51:18 +0000

    add opcaity slider for background image

--- app/app.py
+++ app/app.py
@@ -4,6 +4,10 @@ import plotly.express as px
 import numpy as np
 from PIL import Image  # Import Image module from PIL library
 
+
+# Add a L*a*b  or  LCH value manually to plot on the chatt or upload a cvs file. The csv must have columns L,a,b,Toner
+
+
 # Function to convert L*a*b to LCH
 def lab_to_lch(row):
     L, a, b = row['L'], row['a'], row['b']
@@ -12,7 +16,7 @@ def lab_to_lch(row):
     h_deg = np.degrees(h_rad) % 360
     return L, C, h_deg
 
-st.title("Simple Plotter Lab or LCH values")
+st.title("Simple plotter for LCH or Lab values")
 
 # Sidebar - capture L, C, H and insert into dataframe
 colour_space = st.sidebar.radio("Select Colour Space", ["Lab", "LCH"])
@@ -61,20 +65,30 @@ show_scale = st.sidebar.checkbox("Show Scale", value=True)
 
 # Plotting only if dataframe is not empty
 if not df.empty:
+    
     try:
+
+        #add a slider to control the background image opacity 
+        opacity = st.slider(
+        'Select chart background opacity',
+        0.0, 1.0, (1.0))
+
+
         # Include the Toner as hover data
         fig = px.scatter_polar(df, r="C", theta="H", direction='clockwise', start_angle=-225, text="Toner", height= 600,
-                               hover_data=df.columns)  # dynamically include all columns in hover data
-        fig.update_layout(
+                               hover_data=df.columns, range_r=[0, 100])  # dynamically include all columns in hover data
         
+
+
+        fig.update_layout ( 
         #make transparent
         polar_bgcolor = 'rgba(0,0,0,0.1)',
         paper_bgcolor = 'rgba(0,0,0,0)',
         
+        # Show or hide the radial axis tick labels based on the checkbox
         polar=dict(
-            radialaxis=dict(showticklabels=show_scale)  # Show or hide the radial axis tick labels based on the checkbox
-        ),
-        hoverlabel=dict(bgcolor="white", font_size=14, font_family="Rockwell"),
+            radialaxis=dict(showticklabels=show_scale)),
+            hoverlabel=dict(bgcolor="white", font_size=14, font_family="Rockwell"),
 
         #grab a LCH background image from the web
         images= [dict(
@@ -86,15 +100,14 @@ if not df.empty:
                     yanchor="middle",
                     sizing="stretch",
                     layer="below")], 
-                    margin=dict(l=50, r=50, t=50, b=50)                   
-                             )
+                    margin=dict(l=50, r=50, t=50, b=50))
 
-       
-
+        #update the image and grid
+        fig.update_layout_images(opacity = opacity )
         fig.update_traces(marker=dict(size=8))
         fig.update_traces(textposition='top left')  # Adjust marker size as needed
     
-
+        #plot chart on screen
         st.plotly_chart(fig)
 
     except Exception as e:
