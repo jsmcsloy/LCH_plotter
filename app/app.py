bcd27b5ea57b834a9d5822c4809467598e075ab4
Jamie MCsloy <jmcsloy@gmail.com>
2024-07-23 21:05:08 +0000

    3d app to main

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
 import streamlit as st 
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 from PIL import Image  # Import Image module from PIL library
-
-
-# Add a L*a*b  or  LCH value manually to plot on the chatt or upload a cvs file. The csv must have columns L,a,b,Toner
+import plot_3d as lcm  # Ensure this module is available in your environment
 
 
 # Function to convert L*a*b to LCH
@@ -28,9 +27,9 @@ if colour_space == "Lab":
     ref = st.sidebar.text_input("Enter reference ")
     lab_data = (L, a, b)
     lch_data = lab_to_lch({'L': L, 'a': a, 'b': b})  # Convert Lab to LCH
-    L, C, H = lch_data
+    L_value, C, H = lch_data
 else:
-    L = st.sidebar.number_input("Enter L value", value=50.00)
+    L_value = st.sidebar.number_input("Enter L value", value=50.00)
     C = st.sidebar.number_input("Enter C value", value=50.00)
     H = st.sidebar.number_input("Enter H value", value=180.00)
     ref = st.sidebar.text_input("Enter reference ")
@@ -40,11 +39,8 @@ if 'data_list' not in st.session_state:
     st.session_state.data_list = []
 
 if st.sidebar.button("Add value"):
-    if colour_space == "Lab":
-        st.session_state.data_list.append({'L': L, 'C': C, 'H': H, "Toner": ref})
-    else:
-        st.session_state.data_list.append({'L': L, 'C': C, 'H': H, "Toner": ref})
-    st.sidebar.success("Added: L={}, C={}, H={}, Ref={}".format(L, C, H, ref))
+    st.session_state.data_list.append({'L': L_value, 'C': C, 'H': H, "Toner": ref})
+    st.sidebar.success(f"Added: L={L_value}, C={C}, H={H}, Ref={ref}")
 
 # Load CSV of data points
 data_file = st.sidebar.file_uploader("Load in the CSV file...")
@@ -63,70 +59,119 @@ else:
 # Checkbox to toggle the scale
 show_scale = st.sidebar.checkbox("Show Scale", value=False)
 
-# Plotting only if dataframe is not empty
-if not df.empty:
-    
-    try:
+# Checkbox to toggle the flip to 3D scale
+show_3d = st.sidebar.checkbox("Show in 3D", value=False)
 
-        st.markdown("""---""") 
-        st.write("")
-        #add a slider to control the background image opacity 
-        opacity = st.slider(
-        'Select chart background opacity',
-        0.0, 1.0, (1.0)) 
+if not show_3d: 
+    # Plotting only if dataframe is not empty
+    if not df.empty:
+        try:
+            st.markdown("""---""") 
+            st.write("")
+            # Add a slider to control the background image opacity 
+            opacity = st.slider('Select chart background opacity', 0.0, 1.0, 1.0) 
 
-
-        # Check if "source" column is present in the DataFrame
-        if "Source" in df.columns:
-            fig = px.scatter_polar(df, r="C", theta="H", color="Source", direction='counterclockwise', start_angle= -23, text="Toner", height=600,
-                                hover_data=df.columns, range_r=[0, 130])
-        else:
-            fig = px.scatter_polar(df, r="C", theta="H", direction='counterclockwise', start_angle=-23, text="Toner", height=600,
-                           hover_data=df.columns, range_r=[0, 130])
-        
-        fig.update_layout ( 
-
-        legend=dict(
-        orientation="v",
-        yanchor="bottom",
-        y=1.02,
-        xanchor="center",
-        x=1
-        ),       
-
-       #make transparent
-        polar_bgcolor = 'rgba(0,0,0,0)',
-        paper_bgcolor = 'rgba(0,0,0,0)',
-       
-        # Show or hide the radial axis tick labels based on the checkbox
-        polar=dict(
-            radialaxis=dict(showticklabels=show_scale, visible= show_scale)),
-            hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell"),
-
-        #grab a LCH background image from the web
-        images= [dict(
-                    #source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEwJBlL0BSeVPaLCBKFk15vf-LKej1Kvx5yQ&usqp=CAU",
+            # Check if "source" column is present in the DataFrame
+            if "Source" in df.columns:
+                fig = px.scatter_polar(df, r="C", theta="H", color="Source", direction='counterclockwise', start_angle=-23, text="Toner", height=600,
+                                       hover_data=df.columns, range_r=[0, 130])
+            else:
+                fig = px.scatter_polar(df, r="C", theta="H", direction='counterclockwise', start_angle=-23, text="Toner", height=600,
+                                       hover_data=df.columns, range_r=[0, 130])
+            
+            fig.update_layout( 
+                legend=dict(
+                    orientation="v",
+                    yanchor="bottom",
+                    y=1.02,
+                    xanchor="center",
+                    x=1
+                ),       
+                polar_bgcolor='rgba(0,0,0,0)',
+                paper_bgcolor='rgba(0,0,0,0)',
+                polar=dict(
+                    radialaxis=dict(showticklabels=show_scale, visible=show_scale)
+                ),
+                hoverlabel=dict(bgcolor="white", font_size=16, font_family="Rockwell"),
+                images=[dict(
                     source="https://iili.io/JUVtZog.png",
                     xref="paper", yref="paper",
                     x=0.503, y=0.503,
-                    sizex= 1.0, sizey=1.13,
+                    sizex=1.0, sizey=1.13,
                     xanchor="center",
                     yanchor="middle",
                     sizing="contain",
-                    layer="below")], 
-                    margin=dict(l=50, r=50, t=50, b=50))
+                    layer="below"
+                )],
+                margin=dict(l=50, r=50, t=50, b=50)
+            )
 
-        #update the image and grid
-        fig.update_layout_images(opacity = opacity )
-        fig.update_traces(marker=dict(size=10))
-        fig.update_traces(textposition='top left')  # Adjust marker size as needed
-    
-    
-        #plot chart on screen
-        st.plotly_chart(fig)
-
-            
-    except Exception as e:
-        st.error(f"Error generating plot: {e}")
+            # Update the image and grid
+            fig.update_layout_images(opacity=opacity)
+            fig.update_traces(marker=dict(size=10))
+            fig.update_traces(textposition='top left')  # Adjust marker size as needed
+        
+            # Plot chart on screen
+            st.plotly_chart(fig)
+        except Exception as e:
+            st.error(f"Error generating plot: {e}")
+    else:
+        st.info("Upload data or add LCH values to generate plot.")
 else:
-    st.info("Upload data or add LCH values to generate plot.")
+    if not df.empty:
+        try:
+            L_values = df['L'].tolist()
+            C_values = df['C'].tolist()
+            H_values = df['H'].tolist()
+            
+            # Ensure 'Toner' column exists in the DataFrame
+            Toners = df['Toner'].tolist() if 'Toner' in df.columns else [""] * len(df)
+            
+            # Define marker shapes based on 'Source' values if 'Source' column exists
+            if 'Source' in df.columns:
+                sources = df['Source'].tolist()
+                unique_sources = list(set(sources))
+                symbols = ['circle', 'square', 'diamond', 'cross', 'x', 'star', 'triangle-up', 'triangle-down']
+                source_symbol_map = {source: symbols[i % len(symbols)] for i, source in enumerate(unique_sources)}
+                marker_symbols = [source_symbol_map[source] for source in sources]
+            else:
+                marker_symbols = ['circle'] * len(df)
+
+            df2 = lcm.plot_lch_colors(L_values, C_values, H_values)
+            colours = [i for i in df2[1]]  # Extract color data from the result
+
+            # Generate hover text
+            hover_text = [f'L: {L}<br>C: {C}<br>H: {H}<br>Toner: {toner}'
+                          for L, C, H, toner in zip(L_values, C_values, H_values, Toners)]
+
+            # Create 3D scatter plot
+            fig = go.Figure(data=[go.Scatter3d(
+                x=C_values,
+                y=H_values,
+                z=L_values,
+                mode='markers',
+                marker=dict(
+                    size=7,
+                    color=colours,
+                    opacity=0.8,
+                    symbol=marker_symbols  # Set marker symbols based on source
+                ),
+                text=hover_text,
+                hoverinfo='text'
+            )])
+
+            # Update layout
+            fig.update_layout(
+                scene=dict(
+                    xaxis_title='Chroma (C)',
+                    yaxis_title='Hue (H)',
+                    zaxis_title='Lightness (L)',
+                ),
+                margin=dict(l=0, r=0, b=0, t=0)
+            )
+
+            st.plotly_chart(fig)
+        except Exception as e:
+            st.error(f"Error generating 3D plot: {e}")
+    else:
+        st.info("Upload data or add LCH values to generate plot.")
